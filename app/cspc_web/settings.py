6beb2cc341eb157c635bcd5c324ed711abcb7b60
ljy2855 <ljy2855@naver.com>
2024-01-25 23:57:47 +0900

    build: Add local env

--- app/cspc_web/settings.py
+++ app/cspc_web/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key  
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +24,18 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+if os.environ.get('SECRET_KEY',None) == None:
+    random_key = get_random_secret_key()
+    f = open('.env','a')
+    f.write(f"SECRET_KEY={random_key}")
+    SECRET_KEY = random_key
+else:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","localhost 127.0.0.1").split(" ")
 
 
 REST_FRAMEWORK = {
