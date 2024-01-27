7f612ff602b994d2ceb836e66c3a3106985f0a98
ljy2855 <ljy2855@naver.com>
2024-01-28 00:09:16 +0900

    build: seperate local, deploy debug mode

--- app/cspc_web/settings.py
+++ app/cspc_web/settings.py
@@ -34,7 +34,7 @@ else:
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG_MODE", True)
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split(" ")
 
